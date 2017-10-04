2ddef1e9b0395072037dcb5dfd9662f07555201b
Strudy <aravnarula18@gmail.com>
2017-10-03 20:45:15 -0400

    Create Manifest.js

---
+++ Manifest.js
@@ -0,0 +1,16 @@
+
+
+  "manifest_version": Beta,
+
+  "name": "Trend",
+  "description": "Get Trends search results for your page!",
+  "version": "1.0",
+
+  "browser_action": 
+    "default_popup": "popup.html"
+  },
+  "permissions": [
+    "activeTab",
+    "https://ajax.googleapis.com/"
+  ]
+}
